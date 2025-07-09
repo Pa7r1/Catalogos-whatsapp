bda477638b67250ab666249d766ebe2d255097b5
Pa7r1 <valentincarpio32@gmail.com>
2025-07-08 23:21:54 -0300

    config database

--- api/config.js
+++ api/config.js
@@ -4,10 +4,10 @@ dotenv.config();
 const configDatabase = {
   mysql: {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   },
 };
-
 export default configDatabase;
